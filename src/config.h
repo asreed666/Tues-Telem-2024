6af0789fa59a3b8f900bcfc1f5d7ce9ecfc6bb6c
asreed666 <areed@cityplym.ac.uk>
2024-01-16 11:40:43 +0000

    Reading Temperature

--- src/config.h
+++ src/config.h
@@ -6,4 +6,16 @@
 
 #define BLINKING_RATE   500
 
+/* A constant of NCP18XH103F03RB thermistor is */
+#define A_COEFF                          (float)(0.0009032679f)
+/* B constant of NCP18XH103F03RB thermistor is */
+#define B_COEFF                          (float)(0.000248772f)
+/* C constant of NCP18XH103F03RB thermistor is */
+#define C_COEFF                          (float)(0.0000002041094f)
+
+/* Zero Kelvin in degree C */
+#define ABSOLUTE_ZERO                    (float)(-273.15)
+
+#define PD_RESISTOR (float)(10000)
+
 #endif
--- src/sensors.cpp
+++ src/sensors.cpp
@@ -14,8 +14,17 @@
      gnd = 0;
      float temperatureVoltage;
      while( true ) {
-         temperatureVoltage = tempVolts.read();
-         printf( "The temp Voltage is: %2.2f\n", temperatureVoltage);
+        temperatureVoltage = tempVolts.read() * 2.4; // vref for adc is 1.2 * 2 => 2.4 * reading for voltage
+        float current = (3.3 - temperatureVoltage) / PD_RESISTOR;
+        float thermResistance = temperatureVoltage / current;
+        float logrT = (float32_t)log((float64_t)thermResistance);
+        /* Calculate temperature from the resistance of thermistor using
+        * Steinhart-Hart Equation */
+        float stEqn = (float32_t)((A_COEFF) + ((B_COEFF)*logrT) +
+                                  ((C_COEFF)*pow((float64)logrT, (float32)3)));
+        float temperatureC = (float32_t)(((1.0 / stEqn) + ABSOLUTE_ZERO) + 0.05);
+
+         printf( "The temperature is: %2.2fC\n", temperatureC);
          ThisThread::sleep_for(SENSOR_RATE);
      }
  }
